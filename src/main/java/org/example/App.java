14f263abae6f4962ef133589cf1c80f2ad6aa67d
Ryan <rdturner40@gmail.com>
2021-09-05 14:23:03 -0400

    MVP

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,55 @@
  */
 package org.example;
 
-public class App 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void handleOutput(int largest) {
+        System.out.println("The largest number is: " + largest);
+    }
+    public static void compareValues(int first, int second, int third) throws Exception {
+        if (first > second && first > third) {
+            handleOutput(first);
+        } else if (second > first && second > third) {
+            handleOutput(second);
+        } else if (third > first && third > second) {
+            handleOutput(third);
+        } else {
+            throw new Exception("There seems to be a problem, No number is largest");
+        }
+    }
+
+    public static void getInput() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int first;
+        int second;
+        int third;
+        while (true) {
+            try {
+                System.out.println("Enter the first Number: ");
+                first = Integer.parseInt(reader.readLine());
+
+                System.out.println("Enter the second number: ");
+                second = Integer.parseInt(reader.readLine());
+
+                System.out.println("Enter the third number");
+                third = Integer.parseInt(reader.readLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number");
+            }
+        }
+        if (first == second && first == third) {
+            System.exit(0);
+        } else {
+            compareValues(first, second, third);
+        }
+
+    }
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        getInput();
     }
 }
